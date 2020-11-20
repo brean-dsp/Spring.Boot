50770ffa5ac426301620e1dab03ebc6f38600e6d
Brean de Souza Pinto <brean.dsp@gmail.com>
2020-11-19 21:48:04 -0300

    Inseri o metodo remover telefone para a tela de telefone e o metodo de excluir o objeto Mestre e detalhe na tela principal

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -130,7 +130,7 @@ public class PessoaController {
 	
 	
 //Método que direciona para a página telefones e exibe os números para cada usuário
-@GetMapping("/telefones/{idpessoa}")
+@GetMapping("**/telefones/{idpessoa}")
 	public ModelAndView telefones(@PathVariable("idpessoa") Long idpessoa) {
 		
 		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
@@ -139,6 +139,7 @@ public class PessoaController {
 		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
 		
 		modelAndView.addObject("pessoaobj", pessoa.get());
+		modelAndView.addObject("telefones", telefoneRepository.getTelefones(idpessoa));
 		
 		return modelAndView;
 		
@@ -162,8 +163,28 @@ public class PessoaController {
 //Inícia o objeto e retorna para a página
 		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
 		modelAndView.addObject("pessoaobj", pessoa);
+		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
 		
 		return modelAndView;
 	}
+
+
+//Método que exclui telefone da pagina de telefones
+@GetMapping("/removertelefone/{idtelefone}")
+	public ModelAndView removertelefone(@PathVariable("idtelefone") Long idtelefone) {
+	
+	Pessoa pessoa = telefoneRepository.findById(idtelefone).get().getPessoa();
+	
+	telefoneRepository.deleteById(idtelefone);
+	
+//Inícia o objeto e retorna para a página
+	ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+	
+	modelAndView.addObject("pessoaobj", pessoa);
+	modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoa.getId()));
+	
+	return modelAndView;
+	
+}
 	
 }
