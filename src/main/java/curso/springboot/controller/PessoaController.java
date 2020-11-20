a12314f2a8ba93dab82022d0d201d8d7db972b75
Brean de Souza Pinto <brean.dsp@gmail.com>
2020-11-20 14:31:43 -0300

    Criei a vlidação dos campos de nome, sobrenome e idade e exibindo mensagem de erro

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package curso.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,7 +52,26 @@ public class PessoaController {
 	
 //Método que salva  a pessoa cadastrada e retorna a lista na mesma tela de cadastro
 @RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
-	public ModelAndView salvar(Pessoa pessoa) {
+	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {
+	
+		if(bindingResult.hasErrors()) {
+			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+			
+			Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+			
+			modelAndView.addObject("pessoas", pessoasIt);
+			modelAndView.addObject("pessoaobj", pessoa);
+			
+			List<String> msg = new ArrayList<String>();
+			for(ObjectError objectError : bindingResult.getAllErrors()) {
+				msg.add(objectError.getDefaultMessage()); //vem das anotações @notempty e @notnull
+			}
+			
+			modelAndView.addObject("msg", msg);
+			return modelAndView;
+		}
+	
+	
 		pessoaRepository.save(pessoa);
 		
 	//Inícia o objeto e retorna para a página
--- src/main/java/curso/springboot/model/Pessoa.java
+++ src/main/java/curso/springboot/model/Pessoa.java
@@ -9,6 +9,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Pessoa implements Serializable {
@@ -18,11 +21,19 @@ public class Pessoa implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
+	
+//Anotação que impede do campo estar nulo	
+	@NotNull(message = "Nome não pode ser nulo")
+//Anotação responsável por não permitir um campo vazio, sem dados
+	@NotEmpty(message = "Nome não pode ser vazio")
 	private String nome;
 
+	@NotNull(message = "Sobrenome não pode ser nulo")
+	@NotEmpty(message = "Sobreome não pode ser vazio")
 	private String sobrenome;
 	
+//Anotação de controle de idade	
+	@Min(value = 18, message = "Idade invalida")
 	private int idade;
 	
 	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL)
